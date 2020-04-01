4b09df2703c263d58fb9f39dec32962f50ddbf9d
Marius <madalindinu199@gmail.com>
2020-04-01 19:01:23 +0200

    2n comit

---
+++ Covid-19.java
@@ -0,0 +1,14 @@
+public class Covid-19 extends Coronavirus{
+
+private int DiesQuarentena = 0;
+
+        //Constructors 
+
+        //Per defecte
+
+	// Per Parametres
+
+	//Per Copiadora 
+
+ }
+}
